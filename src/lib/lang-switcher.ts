eab3204dde895cc579a9c0c21993d00f9a0118a0
Dede Ariansya <me@dedeard.my.id>
2024-01-24 10:04:15 +0800

    🐞 fix: html lang attr.

--- src/lib/lang-switcher.ts
+++ src/lib/lang-switcher.ts
@@ -9,7 +9,10 @@ interface AppState {
 const useLanguage = create<AppState>((set) => ({
   loadingLang: false,
   lang: 'en',
-  setLang: (lang: 'en' | 'id') => set(() => ({ lang: lang })),
+  setLang: (lang: 'en' | 'id') => {
+    document.documentElement.lang = lang
+    set(() => ({ lang: lang }))
+  },
 }))
 
 export default useLanguage
