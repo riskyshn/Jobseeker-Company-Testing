f645f4da3e631d5f1d36a0c77e530cf3762ae116
Dede Ariansya <me@dedeard.my.id>
2024-01-29 11:37:38 +0800

    🐞 fix: hero image on wide screen.

--- src/components/pages/AboutUsHero.tsx
+++ src/components/pages/AboutUsHero.tsx
@@ -7,10 +7,10 @@ const AboutUsHero: React.FC = () => {
   return (
     <section className="container py-12 pt-20 md:py-24">
       <div className="flex flex-col items-center justify-center gap-4 md:flex-row-reverse">
-        <div>
+        <div className="md:w-[40%]">
           <Image alt="" src={HeroImage} className="block w-full" />
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 md:flex-1">
           <h2 className="text-4xl font-bold text-secondary md:text-6xl">{tl['about-us']}</h2>
           <p>{tl['about-us-line-1']}</p>
           <p>{tl['about-us-line-2']}</p>
