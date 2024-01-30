168b01c129e2ab1ed0d664f274a2bcdedbe572e6
Dede Ariansya <me@dedeard.my.id>
2024-01-30 09:48:02 +0800

    🐞 fix: text transition stuck.

--- src/components/pages/HomeHero.tsx
+++ src/components/pages/HomeHero.tsx
@@ -3,17 +3,19 @@ import Image from 'next/image'
 import Connecting from '@/assets/home-page/connecting.png'
 import Landing from '@/assets/landing.png'
 
+const textGenerated = ['Opportunity', 'Better Job', 'Better Life']
+
 const HomeHero: React.FC = () => {
-  const [text, setText] = useState('Opportunity')
+  const [textIndex, setTextIndex] = useState(0)
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      const textGenerated = ['Opportunity', 'Better Job', 'Better Life']
-      const nextIndex = (textGenerated.indexOf(text) + 1) % textGenerated.length
-      setText(textGenerated[nextIndex])
+    const timer = setInterval(() => {
+      setTextIndex((prevIndex) => (prevIndex === 2 ? 0 : prevIndex + 1))
     }, 2000)
 
-    return () => clearInterval(intervalId)
+    return () => {
+      clearInterval(timer)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -31,7 +33,7 @@ const HomeHero: React.FC = () => {
         <h1 className="hidden">Connecting people with Opportunities</h1>
         <div className="flex items-center gap-2">
           <span className="text-2xl text-muted md:text-4xl">People with</span>
-          <span className="text-2xl font-semibold text-primary md:text-4xl">{text}.</span>
+          <span className="text-2xl font-semibold text-primary md:text-4xl">{textGenerated[textIndex]}.</span>
         </div>
       </div>
     </section>
